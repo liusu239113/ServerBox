60c22a129ebac57c2edd7926977c59989063cebe
liusu239113 <liusu239113@github.com>
2026-07-06 11:32:04 +0800

    fix: add missing SSHClient extension export for execForOutput

--- lib/data/helper/gen_ssh.dart
+++ lib/data/helper/gen_ssh.dart
@@ -1,6 +1,7 @@
 ﻿/// SSH client generation helper.
-/// Re-exports genClient and Spi for AgentService.
+/// Re-exports genClient, Spi, and SSHClient extensions for AgentService.
 library;
 export 'package:dartssh2/dartssh2.dart' show SSHClient;
+export 'package:surlor_ai/core/extension/ssh_client.dart';
 export 'package:surlor_ai/core/utils/server.dart' show genClient;
 export 'package:surlor_ai/data/model/server/server_private_info.dart' show Spi;
